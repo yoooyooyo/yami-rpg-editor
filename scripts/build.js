863071bb31c7bd6291ef19c8e20fe846d879f530
xiaoxustudio <1783558957@qq.com>
2025-05-01 00:11:55 +0800

    change: yml config

--- scripts/build.js
+++ scripts/build.js
@@ -1,43 +1,14 @@
-const cmd = require("child_process");
 const fs = require("fs");
 const path = require("path");
-const archiver = require("archiver");
+const { build } = require("electron-builder");
 
-function buildZip(type) {
-  const output = fs.createWriteStream(
-    path.join(__dirname, `../build/${type}.zip`)
-  );
-  const archive = archiver("zip", {
-    zlib: { level: 9 }, // 压缩级别
-  });
+const buildConfig = JSON.parse(
+  fs.readFileSync(path.join((__dirname, "./package.json")))
+).build;
 
-  output.on("close", function () {
-    console.log(archive.pointer() + " total bytes");
-    console.log("ZIP 已创建完成！");
-  });
-
-  archive.on("error", function (err) {
-    throw err;
-  });
-  archive.directory(path.join(__dirname, `../build/${type}-unpacked`), type);
-  archive.pipe(output);
-  archive.finalize();
-}
-
-const types = {
-  win: "electron-builder --dir --win --x64",
-  linux: "electron-builder --dir --mac --x64 --no-symlink",
-  mac: "electron-builder --dir --linux --x64",
-  macArm64: "electron-builder --dir --mac --arm64 --no-symlink",
-  universal: "electron-builder --dir --mac --universal",
-};
-
-const args = process.argv;
-
-if (Object.keys(types).includes(args[2])) {
-  const cmdStr = types[args[2]];
-  cmd.exec(cmdStr).on("exit", () => {
-    console.log("开始压缩...");
-    buildZip("win");
-  });
-}
+build({ config: buildConfig }).then(
+  () => console.log("构建完成"),
+  async (error) => {
+    console.error("构建失败:", error);
+  }
+);
